cae719679c7e975c5e32bd8e69e2204ad7f5db04
8DZ8 <d.z.2021@inbox.ru>
2021-11-22 08:49:19 +0300

    Init

---
+++ Dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toUpperCaseFirstLetter = void 0;
+function toUpperCaseFirstLetter(val) {
+    return val[0].toUpperCase() + val.substring(1);
+}
+exports.toUpperCaseFirstLetter = toUpperCaseFirstLetter;
---
+++ Src/TS/index.ts
@@ -0,0 +1,3 @@
+export function toUpperCaseFirstLetter(val: string): string {
+  return val[0].toUpperCase() + val.substring(1);
+}
